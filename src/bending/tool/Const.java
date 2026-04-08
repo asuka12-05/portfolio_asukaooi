1b881fec47f95f96e4046a8630c11b7bf621cf94
C3USER-05\c3user <c3user@c3user.co.jp>
2026-04-08 16:07:15 +0900

    水曜日追加

---
+++ src/bending/tool/Const.java
@@ -0,0 +1,16 @@
+package bending.tool;
+
+public class Const {
+	
+	// 画面URL
+	public static final String URL_START = "start.jsp";
+	public static final String URL_CONSUMER = "consumer.jsp";
+	public static final String URL_ADMIN = "admin.jsp";
+
+	// エラーメッセージ
+	public static final String MSG_SOLD_OUT = "売り切れです";
+	public static final String MSG_NO_MONEY = "金額が不足しています";
+	public static final String MSG_ZERO_SALES = "売上がありません";
+	public static final String MSG_CUSTOM_LIMIT = "カスタム商品は5件が上限です";
+
+}
