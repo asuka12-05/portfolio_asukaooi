cc3fc595b8d9032354b92776ffcd941bd84b2c14
C3USER-05\c3user <c3user@c3user.co.jp>
2026-04-16 16:01:11 +0900

    木曜日追加

--- src/main/java/bending/Servlet/Admin.java
+++ src/main/java/bending/Servlet/Admin.java
@@ -2,11 +2,15 @@ package bending.Servlet;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import bending.tool.Const;
 
 /**
  * Servlet implementation class Admin
@@ -14,23 +18,27 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/bending/admin")
 public class Admin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	
+	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd = request.getRequestDispatcher("/admin.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		dao.replenish(); //在庫補充
-		dao.replace(); //商品入れ替え
-		dao.addCustom(); //カスタム商品作成
-		dao.collect(); //売上回収
+		String action = request.getParameter(Const.PARAM_ACTION);
+		HttpSession session = request.getSession();
+		try {
+			dao.replenish(); //在庫補充
+			dao.replace(); //商品入れ替え
+			dao.addCustom(); //カスタム商品作成
+			dao.collect(); //売上回収
 	}
 
 }
